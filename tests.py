28710038e3c639f2477acc20980d5c2380eaa4db
Anastasia Shurenkova <anastasia.k.shurenkova@yandex.ru>
2024-04-03 21:02:16 +0300

    First commit

--- tests.py
+++ tests.py
@@ -1,24 +1,93 @@
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
-
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
-
-        # добавляем две книги
+        collector.books_genre = {}
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.books_genre) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    def test_add_new_book_len_name_more_than_forty(self):
+         collector = BooksCollector()
+         collector.books_genre = {}
+         collector.add_new_book('Гордость и предубеждение и зомби и вампиры')
+
+         assert len(collector.books_genre) == 0
+
+    def test_set_book_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение')
+        collector.set_book_genre('Гордость и предубеждение', 'Фантастика')
+
+        assert collector.get_book_genre('Гордость и предубеждение') == 'Фантастика'
+
+    def test_get_book_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Незнайка')
+        collector.set_book_genre('Незнайка', 'Мультфильмы')
+
+        assert collector.get_book_genre('Незнайка') == 'Мультфильмы'
+
+
+    def test_get_books_for_children_genre_cartoon(self):
+        collector = BooksCollector()
+        collector.add_new_book('Незнайка')
+        collector.set_book_genre('Незнайка', 'Мультфильмы')
+
+        assert collector.get_books_for_children() == ['Незнайка']
+
+    def test_get_books_for_children_genre_horror(self):
+        collector = BooksCollector()
+        collector.add_new_book('Фредди Крюгер')
+        collector.set_book_genre('Фредди Крюгер', 'Ужасы')
+
+        assert collector.get_books_for_children() == []
+
+    def test_get_books_with_specific_genre_fantasy(self):
+        collector = BooksCollector()
+        collector.add_new_book('Незнайка')
+        collector.set_book_genre('Незнайка', 'Мультфильмы')
+        collector.add_new_book('Гордость и предубеждение')
+        collector.set_book_genre('Гордость и предубеждение', 'Фантастика')
+
+        assert collector.get_books_with_specific_genre('Фантастика') == ['Гордость и предубеждение']
+
+    def test_get_book_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Незнайка')
+        collector.set_book_genre('Незнайка', 'Мультфильмы')
+        collector.add_new_book('Гордость и предубеждение')
+        collector.set_book_genre('Гордость и предубеждение', 'Фантастика')
+
+        assert collector.get_book_genre('Гордость и предубеждение') == 'Фантастика'
+
+    def test_add_book_in_favorites_in_books_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Незнайка')
+        collector.set_book_genre('Незнайка', 'Мультфильмы')
+        collector.add_new_book('Гордость и предубеждение')
+        collector.set_book_genre('Гордость и предубеждение', 'Фантастика')
+        collector.add_book_in_favorites('Незнайка')
+
+        assert collector.get_list_of_favorites_books() == ['Незнайка']
+
+    def test_delete_book_from_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('Незнайка')
+        collector.set_book_genre('Незнайка', 'Мультфильмы')
+        collector.add_new_book('Гордость и предубеждение')
+        collector.set_book_genre('Гордость и предубеждение', 'Фантастика')
+        collector.add_book_in_favorites('Незнайка')
+        collector.add_book_in_favorites('Гордость и предубеждение')
+        collector.delete_book_from_favorites('Незнайка')
+
+        assert collector.get_list_of_favorites_books() == ['Гордость и предубеждение']
+
+    def test_get_list_of_favorites_books_without_books_in_list(self):
+        collector = BooksCollector()
+
+        assert collector.get_list_of_favorites_books() == []
+
